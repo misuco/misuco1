9a941d863880408fa12a1859e0ba4be8e9bc8b94
c1audio <c1audio@framework4.ch>
2013-09-23 23:25:31 +0200

    just modded xdtool script

--- rc1/puredata/xdotool.sh
+++ rc1/puredata/xdotool.sh
@@ -2,15 +2,15 @@
 export DISPLAY=:0.0
 if [ "$2" -eq "127" ]
 then
-if [ "$1" -eq "62" ]
+if [ "$1" -eq "36" ]
 then
     xdotool key F5
 fi
-if [ "$1" -eq "65" ]
+if [ "$1" -eq "37" ]
 then
     xdotool key Left
 fi
-if [ "$1" -eq "67" ]
+if [ "$1" -eq "38" ]
 then
     xdotool key Right 
 fi

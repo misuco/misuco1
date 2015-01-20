6c4ca0c2e01472bb40c2914d0b9b319a5a70333d
Claudio Zopfi <z@x21.ch>
2015-01-20 16:45:04 +0100

    space from border in fps display

--- paint/paintwave.cpp
+++ paint/paintwave.cpp
@@ -34,7 +34,7 @@ void PaintWave::paint(RC1 *rc1, QPainter *pnt)
     }
     pnt->setPen(Qt::black);
     QString fps,freqs;
-    fps.sprintf("%d fps %d/%d voices ",rc1->getFps(),ctl->getNVoices(),ctl->getNVoicesMax());
+    fps.sprintf(" %d fps %d/%d voices ",rc1->getFps(),ctl->getNVoices(),ctl->getNVoicesMax());
     for(int i=ctl->getNVoices()-1;i>=0;i--) {
         fps.append(freqs.sprintf("  %.1f",ctl->getVF(i)));
     }

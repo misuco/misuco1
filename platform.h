7f105253e3c57c1679e98704dcfe9fac7bef00eb
claudio <z@x21.ch>
2015-02-05 22:19:55 +0100

    merge conflict solved

--- platform.h
+++ platform.h
@@ -1,15 +1,9 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
-<<<<<<< HEAD
-//#define RC1_IOS
-#define RC1_PRO
-#define RC1_LINUX
-=======
 #define RC1_IOS
 //#define RC1_PRO
 //#define RC1_LINUX
->>>>>>> 1588e72c65e9fe3e46269254cb6688c269fb9135
 //#define NOGL
 
 #endif // PLATFORM_H
